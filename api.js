6b1c4b1e1dded851cd00c22c11ae1ff142921b0f
edsmund <edsmund@users.noreply.github.com>
2016-02-19 12:56:24 -0200

    Added files via upload

--- api.js
+++ api.js
@@ -28,13 +28,27 @@ MagicKingdom.GetWaitTimes(function(err, data) {
     	global.nome=data[i].name;
         global.tempo=data[i].waitTime;
     	i++;
-        if (i==1){
-            global.conteudo="<li><p>"+global.nome+"</p><span>"+global.tempo+"</span></li>";
+        if(global.tempo<9){
+            if (i==1){
+                global.conteudo="<li><p>"+global.nome+"</p><span>"+global.tempo+"</span></li>";
+            } else {
+                global.conteudo+="<li><p>"+global.nome+"</p><span>"+global.tempo+"</span></li>";
+            } 
+        } else if(global.tempo>9 && global.tempo<35) {
+            if (i==1){
+                global.conteudo="<li><p>"+global.nome+"</p><span class=yellow>"+global.tempo+"</span></li>";
+            } else {
+                global.conteudo+="<li><p>"+global.nome+"</p><span class=yellow>"+global.tempo+"</span></li>";
+            }
         } else {
-            global.conteudo+="<li><p>"+global.nome+"</p><span>"+global.tempo+"</span></li>";
-        } 
+            if (i==1){
+                global.conteudo="<li><p>"+global.nome+"</p><span class=red>"+global.tempo+"</span></li>";
+            } else {
+                global.conteudo+="<li><p>"+global.nome+"</p><span class=red>"+global.tempo+"</span></li>";
+            }
+        }
     }
-    htmlPage="<html><style>ul {list-style:none !important;font-size:14px;width:400px;Padding :0px; margin:0;color:white;font-family: Roboto, Arial, Helvetica, sans-serif;}span{float:left;padding-top:8px; padding-left:11px;background-color:green;margin-left:7px;height:22px;width:19px;margin-top: 4px;}p{float:left;width:350px;margin: 13px 0px 10px 5px;}li{margin-bottom:1px !important;background-color:#00d0d7;height:40px;margin:0;border:1px;padding:2px;}body{margin:0;padding:0;border:0;}</style><body>";
+    htmlPage="<html><style>.red {padding-left: 8px !important; width: 22px !important; background-color:red !important;} .yellow {padding-left: 8px !important; width: 22px !important; color:black !important;background-color:yellow !important;}ul {list-style:none !important;font-size:14px;width:400px;Padding :0px; margin:0;color:white;font-family: Roboto, Arial, Helvetica, sans-serif;}span{-webkit-border-radius: 5px; -moz-border-radius: 5px; border-radius: 5px;float:left;padding-top:8px; padding-left:11px;background-color:green;margin-left:7px;height:22px;width:19px;margin-top: 4px;}p{float:left;width:350px;margin: 13px 0px 10px 5px;}li{ -webkit-border-radius: 5px; -moz-border-radius: 5px; border-radius: 5px;margin-bottom:1px !important;background-color:#00d0d7;height:40px;margin:0;border:1px;padding:2px;}body{margin:0;padding:0;border:0;}</style><body>";
     htmlPage+="<meta name=viewport content=width=device-width, user-scalable=no><ul>";
     htmlPage+=global.conteudo;
     htmlPage+="</ul>";
